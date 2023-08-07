0607661fc5cddaec055bab4484d4357963778ad0
Fawn <sarahmargaret@ymail.com>
2023-08-07 15:49:44 -0400

    added method that adds content to database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,15 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', readwrite);
+  const objStore = tx.objectStore('jate');
+  const req = objStore.put({id: 1, value: content});
+  const res = await req;
+  console.log('data saved to database', res);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
